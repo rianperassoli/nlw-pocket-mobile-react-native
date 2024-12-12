a5bca2e453f95b1efa3c4e54d56aaa77337e4e64
Rian Perassoli <rian.perassoli@gmail.com>
2024-12-12 09:15:14 -0300

    Created Button component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,17 @@
 import { View } from "react-native";
 import { Welcome } from "@/components/welcome";
 import { Steps } from "@/components/steps";
+import { Button } from "@/components/button";
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
       <Steps />
+
+      <Button>
+        <Button.Title>Start</Button.Title>
+      </Button>
     </View>
   );
 }
---
+++ src/components/button/index.tsx
@@ -0,0 +1,50 @@
+import {
+  Text,
+  TextProps,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ActivityIndicator,
+} from "react-native";
+import { IconProps as TablerIconProps } from "@tabler/icons-react-native";
+
+import { styles } from "./styles";
+import { colors } from "@/styles/colors";
+import React from "react";
+
+type ButtonProps = TouchableOpacityProps & {
+  isLoading?: boolean;
+};
+
+function Button({ children, style, isLoading = false, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity
+      style={[styles.container, style]}
+      activeOpacity={0.8}
+      disabled={isLoading}
+      {...rest}
+    >
+      {isLoading ? (
+        <ActivityIndicator size="small" color={colors.gray[100]} />
+      ) : (
+        children
+      )}
+    </TouchableOpacity>
+  );
+}
+
+function Title({ children }: TextProps) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+interface IconProps {
+  icon: React.ComponentType<TablerIconProps>;
+}
+
+function Icon({ icon: Icon }: IconProps) {
+  return <Icon size={24} color={colors.gray[100]} />;
+}
+
+Button.Title = Title;
+Button.Icon = Icon;
+
+export { Button };
