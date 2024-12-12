eb717e61a59d3908e16be99edb2ef9bcf8c48f91
Rian Perassoli <rian.perassoli@gmail.com>
2024-12-12 18:12:59 -0300

    improving styles

--- src/components/place/index.tsx
+++ src/components/place/index.tsx
@@ -30,7 +30,7 @@ export function Place({ data, ...rest }: Props) {
 
       <View style={styles.content}>
         <Text style={styles.name}>{data.name}</Text>
-        <Text style={styles.description}>{data.description}</Text>
+        <Text style={styles.description} numberOfLines={2}>{data.description}</Text>
 
         <View style={styles.footer}>
           <IconTicket size={16} color={colors.red.base} />
